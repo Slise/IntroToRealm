5a475f249fb4e9dd96e6855d8160dcfd82251570
Benson Huynh <quoc@shaw.ca>
2016-02-05 12:22:38 -0800

    Initial Commit

---
+++ IntroToRealm2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntroToRealm2
+//
+//  Created by Benson Huynh on 2016-02-05.
+//  Copyright © 2016 Benson Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IntroToRealm2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IntroToRealm2
+//
+//  Created by Benson Huynh on 2016-02-05.
+//  Copyright © 2016 Benson Huynh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
